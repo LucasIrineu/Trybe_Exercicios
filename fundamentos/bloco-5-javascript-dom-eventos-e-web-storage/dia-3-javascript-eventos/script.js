a56cae99968f42e5fb1cdafc37ac717a1d8e4fc5
Lucas Irineu <lucasirineu119@gmail.com>
2022-05-24 00:38:20 -0300

    exercicios parte II

--- fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
+++ fundamentos/bloco-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/script.js
@@ -10,20 +10,52 @@ const myWebpage = document.getElementById('my-spotrybefy');
 // 1.2. Note que uma das caixas está um pouco acima das outras. Por que isso ocorre?
 
 // 2. Crie uma função que adicione a classe 'tech' ao elemento `li` quando for clicado.
-function addTechClassOnClick(origin) {
-    
-}
+
 // 2.1. Deve existir apenas um elemento com a classe 'tech'. Como você faz isso?
+
+
+
+function changeToTechClass(origin){
+  let techChange = document.querySelector('.tech').classList;
+  techChange.remove('tech');
+  origin.target.classList.add('tech');
+  
+}
+
+firstLi.addEventListener('click', changeToTechClass);
+secondLi.addEventListener('click', changeToTechClass);
+thirdLi.addEventListener('click', changeToTechClass);
 
 // 3. Crie uma função que, ao digitar na caixa de texto, altere o texto do elemento
 // com a classe 'tech';
 
+function inputToTech(origin){
+  techElement = document.querySelector('.tech');
+  techElement.innerText = origin.target.value;
+}  
+
+input.addEventListener('input', inputToTech);
 // 4. Crie uma função que, ao clicar duas vezes em 'Meu top 3 do Spotrybefy', ele
 // redirecione para alguma página;
 // 4.1. Que tal redirecionar para seu portfólio?
+function openNewPage(){
+  document.querySelector('#my-spotrybefy').target = window.open('https://lucasirineu.github.io/', '_blank');
+}
+
+myWebpage.addEventListener('dblclick', openNewPage);
 
 // 5. Crie uma função que, ao passar o mouse sobre 'Meu top 3 do Spotrybefy', altere
 // a cor do mesmo;
+function mouseOverColor() {
+  document.querySelector('#my-spotrybefy').style.color = '#2fc18c';
+}
+
+function mouseOutColor() {
+  document.querySelector('#my-spotrybefy').style.color ='white';
+}
+
+myWebpage.addEventListener('mouseover', mouseOverColor);
+myWebpage.addEventListener('mouseout', mouseOutColor);
 
 // Segue abaixo um exemplo do uso de event.target:
 
